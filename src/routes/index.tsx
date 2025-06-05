5de34112a1caaaf4b762e2d48475e12f24b065fb
fonces <fonces.xyz@gmail.com>
2025-06-06 02:32:25 +0900

    feat: Routingの追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,70 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { Overlay } from "@/components/atoms/Overlay";
 import { Group } from "@/components/molecules/Group/Component";
+import type { TextProps } from "@/components/Text";
 
 console.log("著作権よくわかりませんすみません。");
 
 const Index: React.FC = () => {
+  const navigate = useNavigate({ from: "/" });
+  const createToNavigate = (to: string) => () => navigate({ to });
+  const items: TextProps[] = [
+    {
+      children: "SKILL",
+      italic: 1,
+      order: 1,
+      size: 60,
+      onClick: createToNavigate("/skill"),
+    },
+    {
+      children: "ITEM",
+      italic: 25,
+      brightness: 80,
+      x: 0.5,
+      onClick: createToNavigate("/item"),
+    },
+    { children: "EQUIP", italic: 25, onClick: createToNavigate("/equip") },
+    {
+      children: "PERSONA",
+      italic: 30,
+      order: 3,
+      size: 63,
+      x: -1,
+      onClick: createToNavigate("/persona"),
+    },
+    {
+      children: "STATUS",
+      italic: 55,
+      brightness: 50,
+      onClick: createToNavigate("/status"),
+    },
+    {
+      children: "QUEST",
+      italic: 35,
+      brightness: 80,
+      x: -0.8,
+      onClick: createToNavigate("/quest"),
+    },
+    { children: "COMMU", italic: 40, disabled: true },
+    {
+      children: "CALENDAR",
+      italic: 35,
+      order: 2,
+      x: -1.4,
+      onClick: createToNavigate("/calendar"),
+    },
+    {
+      children: "SYSTEM",
+      italic: 70,
+      size: 40,
+      brightness: 60,
+      onClick: createToNavigate("/system"),
+    },
+  ];
+
   return (
     <Overlay>
-      <Group
-        items={[
-          { children: "SKILL", italic: 1, order: 1, size: 60 },
-          { children: "ITEM", italic: 25, brightness: 80, x: 0.5 },
-          { children: "EQUIP", italic: 25 },
-          {
-            children: "PERSONA",
-            italic: 10,
-            order: 3,
-            size: 63,
-            x: -1,
-          },
-          { children: "STATUS", italic: 55, brightness: 50 },
-          { children: "QUEST", italic: 35, brightness: 80, x: -0.8 },
-          { children: "COMMU", italic: 40, disabled: true },
-          { children: "CALENDAR", italic: 35, order: 2, x: -1.4 },
-          {
-            children: "SYSTEM",
-            italic: 70,
-            size: 40,
-            brightness: 60,
-          },
-        ]}
-      />
+      <Group items={items} />
     </Overlay>
   );
 };
