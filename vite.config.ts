5351ca148a2cf97c07ac6ea6b8d65623b74f5710
fonces <51437276+fonces@users.noreply.github.com>
2025-06-05 16:19:56 +0000

    chore: lint & fix Viteプラグイン設定含む全体の自動修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+// @ts-expect-error: 型定義がまだ提供されていません
+import { tanstackRouterVitePlugin } from "@tanstack/router-vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tanstackRouterVitePlugin({ target: 'react', autoCodeSplitting: true })],
   base: "/p3r-ui",
 });
